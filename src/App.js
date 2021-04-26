bb73cadcea7edfe839a8db57071d8bf6ce768118
Souhaib Chadidi <chadidi02@localhost.localdomain>
2021-04-26 16:42:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/Navbar/Header";
+import Search from "./components/Search/Search";
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4">
+				<Header />
+				<Search />
+			</div>
+		);
+	}
 }
 
 export default App;
